4e986b1afa7daa4c05315c6f88a5608ec04dd38e
Ruben Iritsyan <treyan94@protonmail.com>
2023-08-12 16:20:17 +0400

    Add chat channel to broadcast routes
    A new chat channel, 'user.{userId}.chat', was added to the broadcast routes in channels.php. This channel allows for direct user-to-user communication, validating the chat by comparing the user id's.

--- routes/channels.php
+++ routes/channels.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Broadcast;
 */
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+    return (int)$user->id === (int)$id;
 });
+
+Broadcast::channel('user.{userId}.chat', static fn(User $user, int $userId) => $user->id === $userId);
