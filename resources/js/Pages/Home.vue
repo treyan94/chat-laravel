9e84e69682fa967f4b078598de678572013ae37e
Ruben Iritsyan <treyan94@protonmail.com>
2023-08-13 11:54:22 +0400

    Add ChatWidget to Home.vue
    ChatWidget component has been added to Home.vue to facilitate user communication. The ChatWidget is now directly imported into Home.vue and rendered on the page, passing the user object as property. This change will allow the user to engage in conversations directly from the home page.

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,4 +1,6 @@
 <script setup>
+import ChatWidget from "../Components/ChatWidget.vue";
+
 defineProps({
     user: {
         type: Object,
@@ -9,6 +11,7 @@ defineProps({
 
 <template>
     <div class="flex items-center justify-center h-screen text-center bg-gradient-to-r from-gray-300 via-gray-400 to-gray-500 text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">
+        <ChatWidget :user="user" />
         <p class="font-serif">Greetings, {{ user.name }}!</p>
     </div>
 </template>
