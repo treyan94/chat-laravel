73b01b73bd62fd2dc29609d678722e3759ab79c7
Ruben Iritsyan <treyan94@protonmail.com>
2023-08-15 17:18:08 +0400

    Add GPT message helper and attribute to chat room model
    This commit adds a helper method - addGptMessage() and an attribute - hasGptUser() to the ChatRoom model. These additions are to enable automation for GPT-3 chat messages. The addGptMessage() method dispatches a new job to add GPT-3 messages while the hasGptUser() refers to a cached attribute to check if a GPT-3 user exists in the chat room.

--- app/Models/ChatRoom.php
+++ app/Models/ChatRoom.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use App\Jobs\AddGptMessageJob;
+use Cache;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -30,5 +33,26 @@ class ChatRoom extends Model
     public function users(): BelongsToMany
     {
         return $this->belongsToMany(User::class);
+    }
+
+
+    /* Helpers */
+
+    public function addGptMessage(): void
+    {
+        AddGptMessageJob::dispatch($this);
+    }
+
+    /* Attributes */
+
+    public function hasGptUser(): Attribute
+    {
+        $cacheKey = 'chat-room:' . $this->id . ':has-gpt-user';
+        $ttl = 60 * 60 * 24;
+        $fn = fn() => $this->users()->where('email', config('openai.gpt_user_email'))->exists();
+
+        return new Attribute(
+            get: fn() => Cache::remember($cacheKey, $ttl, $fn),
+        );
     }
 }
