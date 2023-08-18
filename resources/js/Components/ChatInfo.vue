e4e5cfb0e0086bf8380b5468e66e8dffc2ebc53a
Ruben Iritsyan <treyan94@protonmail.com>
2023-08-18 21:07:30 +0400

    Add ChatInfo component to display room users
    This commit introduces a new Vue.js component - ChatInfo.vue. Its goal is to display information about all users currently present in a chat room. It takes an array of objects ('room.users') as a property and iteratively renders div elements with user names. This addition helps to improve user interaction and overall experience within the chat platform.

---
+++ resources/js/Components/ChatInfo.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {computed} from "vue";
+
+const props = defineProps({
+    room: {
+        type: Object,
+        required: true,
+    },
+})
+
+const users = computed(() => props.room.users);
+</script>
+
+<template>
+    <div>
+        <div class="flex flex-col-reverse mt-4 px-4">
+            <div
+                class="flex items-center mt-1.5 mb-1.5 px-4 py-2 rounded-lg bg-gray-100"
+                v-for="user in users"
+                :key="user.id"
+            >
+                <span class="text-sm font-medium">{{ user.name }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
