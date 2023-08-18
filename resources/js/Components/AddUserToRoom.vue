51d8165558553146f43d591e06dfcfb81f25b3b3
Ruben Iritsyan <treyan94@protonmail.com>
2023-08-18 15:17:59 +0400

    Add AddUserToRoom.vue component
    Implemented a new Vue component, AddUserToRoom.vue, designed to facilitate the option of adding users to a chat room. The component includes a button for adding users and a dropdown for selecting the particular user. Moreover, it handles the API calls to add selected users to the chat room, making sure that the same user cannot be added twice. The users already present in the room are filtered out from the dropdown list.

---
+++ resources/js/Components/AddUserToRoom.vue
@@ -0,0 +1,70 @@
+<script setup>
+import {computed, ref} from "vue";
+import {useToast} from "vue-toastification";
+
+const toast = useToast();
+
+const props = defineProps({
+    users: {
+        type: Array,
+        required: true,
+    },
+    room: {
+        type: Object,
+        required: true,
+    },
+});
+
+const emit = defineEmits(['addUser']);
+
+const openUserDropdown = ref(false);
+
+// filter out the users that are already in the room
+const filteredUsers = computed(() => props.users.filter(user => {
+    return !props.room.users.some(u => u.id === user.id);
+}));
+
+const addUserToRoom = async (user) => {
+    await axios.post(`/chat-rooms/${props.room.id}/users`, {
+        users: [user.id],
+    }).then(() => {
+        // handle success
+        emit('addUser', user);
+        openUserDropdown.value = false;
+    }).catch(error => {
+        toast.error(error.response.data.message);
+    });
+};
+</script>
+
+<template>
+    <div class="relative">
+        <button
+            class="add-user-btn text-white bg-blue-500 px-4 py-2 rounded"
+            @click="openUserDropdown = !openUserDropdown"
+        >
+            Add User
+        </button>
+        <ul
+            v-if="openUserDropdown"
+            class="user-dropdown absolute right-0 mt-2 w-48 bg-white border rounded shadow-xl z-10 overflow-y-auto max-h-48"
+        >
+            <li
+                v-for="user in filteredUsers"
+                :key="user.id"
+                class="border-b border-gray-100"
+            >
+                <button
+                    class="user-option block px-4 py-2 text-gray-800 hover:bg-blue-500 hover:text-white w-full text-left"
+                    @click="addUserToRoom(user)"
+                >
+                    {{ user.name }}
+                </button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
